56f1e9e76b3ebba930d48f1237a188a355665c96
Harsh Kumar <harshkrsharma.22@gmail.com>
2026-09-19 17:41:29 +0530

    Add logging to ingestion pipeline

--- ingest.py
+++ ingest.py
@@ -1,5 +1,6 @@
 from google.cloud import bigquery
 import pandas as pd
+import logging
 
 PROJECT_ID = "central-rush-450208-g6"
 DATASET_ID = "irctc_realtime"
@@ -7,23 +8,37 @@ TABLE_ID = "train_data"
 
 CSV_FILE = "train_data.csv"
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 client = bigquery.Client(project=PROJECT_ID)
 
 table_ref = f"{PROJECT_ID}.{DATASET_ID}.{TABLE_ID}"
 
-df = pd.read_csv(CSV_FILE)
+try:
+    logging.info("Reading train data from CSV...")
+    df = pd.read_csv(CSV_FILE)
 
-job_config = bigquery.LoadJobConfig(
-    write_disposition="WRITE_APPEND"
-)
+    logging.info(f"Rows found: {len(df)}")
 
-job = client.load_table_from_dataframe(
-    df,
-    table_ref,
-    job_config=job_config
-)
+    job_config = bigquery.LoadJobConfig(
+        write_disposition="WRITE_APPEND"
+    )
 
-job.result()
+    logging.info("Loading data into BigQuery...")
 
-print("✅ Train data successfully loaded into BigQuery!")
-print(f"📊 Rows loaded: {len(df)}")
+    job = client.load_table_from_dataframe(
+        df,
+        table_ref,
+        job_config=job_config
+    )
+
+    job.result()
+
+    logging.info("Train data successfully loaded into BigQuery!")
+    logging.info(f"Rows loaded: {len(df)}")
+
+except Exception as e:
+    logging.error(f"Pipeline failed: {e}")
